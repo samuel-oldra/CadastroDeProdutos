c3df4f4eb1aea5b2d573288b9210eac5dcc23f19
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-07 08:15:15 -0300

    CMD: ng g c components/template/header (ou ng generate component ...)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
