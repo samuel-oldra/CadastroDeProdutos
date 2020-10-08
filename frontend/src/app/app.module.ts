23f815faaf4cba06a91396deb40f2ac877c26aae
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-08 13:12:09 -0300

    CMD: ng g c components/template/nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
