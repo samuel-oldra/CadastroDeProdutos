55d0d2a63a370bcf044ba058dac49955ea81f7e4
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-22 00:00:00 -0300

    CMD: ng g d directives/red (ou ng generate directive ...)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
