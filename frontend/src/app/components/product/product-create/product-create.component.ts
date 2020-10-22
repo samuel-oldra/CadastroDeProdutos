9111a982b65cc99c7fa5c5552c98f0deca2fab21
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-22 09:35:05 -0200

    Exemplo de bindings de (evento) e de [atributo]

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  propLegal = "qualquer";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!');
   }
 
 }
