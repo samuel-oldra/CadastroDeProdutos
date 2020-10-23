4e7f467172572522d1ab3b18d4d8946fe02d53fd
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-23 00:00:00 -0300

    Exemplo de uma diretiva de atributo

--- frontend/src/app/directives/red.directive.ts
+++ frontend/src/app/directives/red.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appRed]'
 })
 export class RedDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.color = '#e35e6b';
+  }
 
 }
