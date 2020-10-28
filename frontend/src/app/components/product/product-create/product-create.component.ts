9579825ce808d52469c2ae724b6bfd517592c98e
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-10-28 09:24:16 -0200

    Adicionado formulário para criar produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -11,8 +11,8 @@ import { ProductService } from '../product.service';
 export class ProductCreateComponent implements OnInit {
 
   product: Product = {
-    name: 'Produto de Teste',
-    price: 125.98
+    name: '',
+    price: null
   }
 
   constructor(
