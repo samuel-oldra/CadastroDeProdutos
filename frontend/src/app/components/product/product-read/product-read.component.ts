0d68d943ce97164d10d19cb7c0a4c5aed66f88ca
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-11-05 09:22:13 -0200

    Adicionada coluna de ações na tabela de produtos

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[]
-  displayedColumns = ['id', 'name', 'price']
+  displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor(private productService: ProductService) { }
 
