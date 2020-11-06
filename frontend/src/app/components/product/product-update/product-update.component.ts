3e0d02daae397ed328ee9a1fa7195d08c3a744eb
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-11-06 09:12:03 -0200

    CMD: ng g c components/product/product-update

---
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
