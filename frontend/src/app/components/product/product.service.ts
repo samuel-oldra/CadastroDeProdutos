b9897a452cd251467fc3847c09884170b1ab5a83
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-11-11 00:00:00 -0300

    Adicionados métodos para obter produto e atualizar produto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -32,4 +32,14 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
 }
