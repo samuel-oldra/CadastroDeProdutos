349a9e6d248d86b9d4e09fe398f7248246490711
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-11-11 09:12:02 -0200

    Adicionadas funcionalidades para atualizar os títulos

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) { }
 
   ngOnInit(): void {
+  }
+
+  get title(): string {
+    return this.headerService.headerData.title
+  }
+
+  get icon(): string {
+    return this.headerService.headerData.icon
+  }
+
+  get routeUrl(): string {
+    return this.headerService.headerData.routeUrl
   }
 
 }
--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      routeUrl: ''
+    }
+  }
 
   ngOnInit(): void {
   }
